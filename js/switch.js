90a43d36957aaf155a629b6a1624ea45eb2c46df
Emma Cluff <10782819@AD.UVU.EDU>
2016-09-16 11:58:03 -0600

    deleted a thing

--- js/switch.js
+++ js/switch.js
@@ -1,24 +1,5 @@
 //Create a string var
 
-/*var petName = prompt("What's my pets name?", "Type name here.");
-
-switch (petName) {
-    case "Rover":
-        alert("My dogs name is " + petName); 
-        break;
-   
-   case "Sally":
-        alert("My friends dog is named " + petName); 
-        break;
-        
-    case "Fluffy":
-        alert("A dog named " + petName + " bit me once."); 
-        break;
-        
-    default:
-    alert("That is not my pets name.");
-        break;
-}*/
 
 var gameLevel =  1;
 var password = "Mellon";
